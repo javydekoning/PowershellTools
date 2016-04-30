ffed0f502009b82ea52a0fe2a5eb24a0dc72d9cd
root <root@DESKTOP-N6PP74M.localdomain>
2016-04-30 13:31:29 +0000

    Get-EnumValue

---
+++ Get-EnumValue.psm1
@@ -0,0 +1,74 @@
+﻿function Get-EnumValue
+{ 
+  param (
+    [type]$type,
+    [switch]$object,
+    [switch]$list
+  )
+
+  if ($object)
+  {
+    [enum]::getNames($type) | 
+    Select-Object -Property @{
+      name       = 'Name'
+      expression = {
+        $_
+      }
+    }, 
+    @{
+      name       = 'Value'
+      expression = {
+        $type::$_.value__
+      }
+    }, 
+    @{
+      name       = 'Binary'
+      expression = {
+        [Convert]::ToString($type::$_.value__, 2)
+      }
+    }, 
+    @{
+      name       = 'Hex'
+      expression = {
+        [Convert]::ToString($type::$_.value__, 16).ToUpper()
+      }
+    }
+  }
+  elseif ($list)
+  {
+    [enum]::getNames($type)
+  }
+  else
+  {
+    [enum]::getNames($type) |             
+    Select-Object -Property @{
+      name       = 'Name'
+      expression = {
+        $_
+      }
+    }, 
+    @{
+      name       = 'Value'
+      expression = {
+        $type::$_.value__
+      }
+    }, 
+    @{
+      name       = 'Binary'
+      expression = {
+        [Convert]::ToString($type::$_.value__, 2)
+      }
+    }, 
+    @{
+      name       = 'Hex'
+      expression = {
+        [Convert]::ToString($type::$_.value__, 16).ToUpper()
+      }
+    } | 
+    Format-Table -AutoSize
+  }
+}
+
+
+
+
