ca90013f814934423333d9d771c69d2e36f6270c
root <root@DESKTOP-N6PP74M.localdomain>
2016-04-30 13:26:51 +0000

    ADSI user-unlock

---
+++ Unlock-User.ps1
@@ -0,0 +1,25 @@
+﻿<#
+    .Synopsis
+    Function to unlock user that does not depend on PoSH AD module nor ADWS being available. 
+    .DESCRIPTION
+    Function to unlock user that does not depend on PoSH AD module nor ADWS being available. 
+    .EXAMPLE
+    Unlock-User jdekoning
+#>
+function Unlock-User
+{
+  [CmdletBinding()]
+  Param
+  (
+    #Username
+    [Parameter(Mandatory=$true,Position=0)]
+    $samaccountname
+  )
+  $User = [adsi]([adsisearcher]"samaccountname=$samaccountname").findone().path;
+  if ($user) {
+    $User.psbase.InvokeSet('IsAccountLocked',$false)
+    $User.SetInfo()  
+  } else {
+    Write-Warning "$samaccountname not found."
+  }
+}
