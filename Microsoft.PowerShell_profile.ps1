633b91b7fb501e46797a8790d17fd44c17005095
Javy de Koning <jdekoning@ipsoft.com>
2016-06-05 16:03:38 +0200

    Adding profile for DSC

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 ﻿function prompt {
    "[JavydeKoning.com] $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))"
 }
+
+ls $home\github\PowershellTools\ *.psm1 | % {import-module $_.fullname}
